17d355ff4c6a3c211455f49b1f9aef8c9bda37b5
phamthihoaithu <thunguyet1662002@gmail.com>
2024-10-17 21:50:15 +0700

    Create_Project

---
+++ PhamThiHoaiThu826/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PhamThiHoaiThu826.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
